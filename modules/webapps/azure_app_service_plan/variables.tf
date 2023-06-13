d72ced88fc53e7a26baf3cf401182945caf2ef27
Ali Arslan <aliarslanmushtaqali@gmail.com>
2023-06-13 10:00:28 +0500

    Added Module for ASP

---
+++ modules/webapps/azure_app_service_plan/variables.tf
@@ -0,0 +1,19 @@
+//------------------------------------General Properties------------------------------------\\
+variable "rgname" {
+  type    = string
+  default = "RG-git-DEMO-LINUXVM"
+}
+
+variable "location" {
+  type    = string
+  default = "West US"
+}
+variable "aspname" {
+  type    = string
+  default = "RG-git-DEMO-LINUXVM"
+}
+
+variable "skusize" {
+  type    = string
+  default = "S1"
+}
