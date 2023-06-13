bfbedfe85b6d55fdf66534519d2625fdc180a145
Ali Arslan <aliarslanmushtaqali@gmail.com>
2023-06-13 10:00:15 +0500

    Added module for Azure App Service

---
+++ modules/webapps/azure_app_service/variables.tf
@@ -0,0 +1,19 @@
+//------------------------------------General Properties------------------------------------\\
+variable "rgname" {
+  type    = string
+  default = "RG-git-DEMO-LINUXVM"
+}
+
+variable "location" {
+  type    = string
+  default = "West US"
+}
+variable "appname" {
+  type    = string
+  default = "RG-git-DEMO-LINUXVM"
+}
+
+variable "aspid" {
+  type = string
+  default = ""
+}
