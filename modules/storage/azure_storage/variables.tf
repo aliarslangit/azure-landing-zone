35b93e359bc0708946e3b4d56fab2e6d17b9838a
Ali Arslan <aliarslanmushtaqali@gmail.com>
2023-06-26 08:50:48 +0500

    Update variables.tf

--- modules/storage/azure_storage/variables.tf
+++ modules/storage/azure_storage/variables.tf
@@ -1,7 +1,7 @@
 //------------------------------------General Properties------------------------------------\\
 variable "rgname" {
   type    = string
-  default = "RG-git-DEMO-LINUXVM"
+  default = "Rg-git"
 }
 
 variable "location" {
@@ -10,7 +10,7 @@ variable "location" {
 }
 variable "storagename" {
   type    = string
-  default = "RG-git-DEMO-LINUXVM"
+  default = "Rg-git"
 }
 
 variable "accounttype" {
